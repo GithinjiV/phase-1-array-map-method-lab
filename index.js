7d5ef1f143c2eaf9510fbe205593b189dfa285eb
vinnie <githinjigicheru146@gmail.com>
2024-02-01 10:44:33 +0300

    Tests passednpm test

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((line) => {
+    const words = line.split(" ");
+    const capitalizedWords = words.map(
+      (word) => word.charAt(0).toUpperCase()+ word.slice(1)
+      );
+    const response = capitalizedWords.join(" ");
+    return response;
+  }
+  )
+  
 }
+
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split(" ");
+//     const capitalizedTokens = tokens.map(
+//       (token) => token.charAt(0).toUpperCase() + token.slice(1)
+//     );
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   });
+// };
